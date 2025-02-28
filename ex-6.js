7e27cdf025ebd7ed748476593b80f1075b6ad9c3
unknown <Prachaya>
2025-02-28 14:04:56 +0700

    loop

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,18 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+// วิธีที่ 1
+for (let i = companyName.length-1 ; i >= 0; i--) {
+    reversedCompanyName += companyName[i];
+}
+console.log(reversedCompanyName)
 
-console.log(reversedCompanyName);
+// วิธีที่ 2
+for (let i = 0; i < companyName.length; i++) {
+    console.log(companyName[i])
+    for (let j = 0; j < companyName.length; j++) {
+        reversedCompanyName = companyName[j-i]
+    }
+    console.log(reversedCompanyName)
+} 
+
